dca06b655e290f5b20330ca96a358f44683d418c
rebeccabartels <rebeccakbartels@icloud.com>
2016-07-08 13:04:34 -0400

    viewDidLoad, viewDidAppear, and viewWillAppear

--- ViewLifeCycle/ViewLifeCycle/ViewController.swift
+++ ViewLifeCycle/ViewLifeCycle/ViewController.swift
@@ -5,18 +5,19 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
+    @IBOutlet var ViewController: UIView!
+    override func viewDidLoad() {   // runs only once to see if app opened.
         super.viewDidLoad()
         print("viewDidLoad() called")
         view.backgroundColor = UIColor.randomColor()
     }
 
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(animated: Bool) {    //runs everytime before view appears
         super.viewWillAppear(animated)
         print("viewWillAppear(_:) called")
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(animated: Bool) {  //runs everytime after view appears.
         super.viewDidAppear(animated)
         print("viewDidAppear(_:) called")
     }
